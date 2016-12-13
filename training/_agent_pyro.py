aebad492cef820d5b447c9739e1748228db9c487
Farshid Tavakolizadeh <farshid.tz@gmail.com>
2016-12-13 16:25:26 +0100

    changes to interfacing data structures

--- training/_agent_pyro.py
+++ training/_agent_pyro.py
@@ -2,7 +2,6 @@
 Training / Prediction Agent
 """
 import Pyro4
-import copy
 import numpy as np
 import random
 import matplotlib.pyplot as plt
@@ -14,27 +13,32 @@ from _converter import SensorThings2Dict
 from _evaluation import print_metrics
 import _models
 
-@Pyro4.expose
+
 class Agent(object):
 
-    def build(self, config):
-        self.model = config["model"]
-        self.model_conf = config["model_conf"]
-        self.trained_model = config["trained_model"]
+    @Pyro4.oneway
+    def build(self, classifier):
+        print("build: %s" % classifier)
+        self.clf_name = classifier["name"]
+        self.clf_conf = classifier["conf"]
+        self.model_dir = ""
+        if "dir" in classifier:
+            self.model_dir = classifier["dir"]
         self.corrects = 0
         self.wrongs = 0
 
-
-    def pre_train(self, trainfiles):
-        if path.isfile(self.trained_model+'/model.pkl'):
-            print("Loading pre-trained model from disk...")
-            self.clf = joblib.load(self.trained_model+'/model.pkl')
-            self.means = joblib.load(self.trained_model+'/means.pkl')
+    @Pyro4.oneway
+    def pre_train(self, training_files):
+        print("pre_train: %s" % training_files)
+        if self.model_dir != "" and path.isfile(self.model_dir+'/model.pkl'):
+            self.clf = joblib.load(self.model_dir+'/model.pkl')
+            self.means = joblib.load(self.model_dir+'/means.pkl')
+            print("Loaded pre-trained model from disk.")
             return
 
         data = []
         bad = 0
-        for filename in trainfiles:
+        for filename in training_files:
             print("Loading rows... {}".format(filename))
             with open(filename) as f:
                 for line in f:
@@ -63,7 +67,7 @@ class Agent(object):
 
         self.means = np.mean(not_faulty[:,2:-1].astype(np.float32), axis=0)
         # save means to disk
-        joblib.dump(self.means, self.trained_model+'/means.pkl')
+        joblib.dump(self.means, self.model_dir+'/means.pkl')
 
         """ down/up sample data """
         samples = np.random.choice(len(not_faulty), 5000, replace=False)
@@ -82,19 +86,22 @@ class Agent(object):
 
         """ train model """
         # dynamically call the model
-        clf = getattr(_models, self.model)(self.model_conf)
+        clf = getattr(_models, self.clf_name)(self.clf_conf)
         clf = clf.fit(train_data, train_labels)
         print_metrics(train_labels, clf.predict(train_data))
         print_metrics(test_labels, clf.predict(test_data))
         self.clf = clf
         # save model to disk
-        joblib.dump(clf, self.trained_model+'/model.pkl')
+        joblib.dump(clf, trained_model_dir+'/model.pkl')
 
+    @Pyro4.oneway
     def learn(self, datapoint):
-        print(datapoint)
+        print("learn: %s" % datapoint)
 
-
+    @Pyro4.expose
     def predict(self, datapoint):
+        print("predict: %s" % datapoint)
+        return 1
         features = SensorThings2Dict(datapoint, complete=False)
         features = np.array(features.values())
         # convert measurements to numpy array
@@ -119,9 +126,10 @@ class Agent(object):
 
         return {'prediction': p.item()}
 
+    @Pyro4.expose
     def echo(self, msg):
-        print("echo: %s"%msg)
-        return "agent.echo: " + msg
+        #print("echo: %s"%msg)
+        return msg
 
 # Start Pyro
 Pyro4.config.SERIALIZER = 'pickle'
