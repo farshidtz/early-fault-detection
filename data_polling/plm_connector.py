a584940b43a825c2e51bac0844b8249daa98dc19
Farshid Tavakolizadeh <farshid.tz@gmail.com>
2016-12-23 17:58:06 +0100

    refactoring

--- data_polling/plm_connector.py
+++ data_polling/plm_connector.py
@@ -17,26 +17,24 @@ MQTT_BROKER_PORT = 1883
 MQTT_CLIENTID_PREFIX = "plm_connector_"
 MQTT_SUBSCRIBE_TOPIC = "/outgoing/#"
 
-# Setup Socket writer
+# Setup sockets
+sock_reader = SocketReader(SOCKET_HOST, SOCKET_DATA_PORT)
 sock_writer = SocketWriter(SOCKET_HOST, SOCKET_FEEDBACK_PORT)
-sock_writer.send("test")
-def outgoingHandler(json_obj):
-    sock_writer.send(json_obj)
-
-# Setup MQTT publisher
+# Setup MQTT clients
 publisher = MQTTPublisher(MQTT_BROKER_HOST, MQTT_BROKER_PORT, MQTT_CLIENTID_PREFIX+str(time.time()))
 subscriber = MQTTSubscriber(MQTT_BROKER_HOST, MQTT_BROKER_PORT, MQTT_CLIENTID_PREFIX+str(time.time()))
-subscriber.subscribe(MQTT_SUBSCRIBE_TOPIC, outgoingHandler, qos=0)
 
+
+def outgoingHandler(json_obj):
+    sock_writer.send(json_obj)
 
 def incomingHandler(json_obj):
     publisher.publishSENML(json_obj[json_obj.keys()[0]], qos=0)
     sys.stdout.flush()
 
-# Setup Socket reader
-sock_reader = SocketReader(SOCKET_HOST, SOCKET_DATA_PORT)
 sock_reader.start(incomingHandler)
-
+sock_writer.send("ping")
+subscriber.subscribe(MQTT_SUBSCRIBE_TOPIC, outgoingHandler, qos=0)
 
 while True:
     try:
@@ -45,4 +43,5 @@ while True:
         print "\nInterrupted."
         sock_reader.stop()
         subscriber.stop()
+        sys.stdout.flush()
         sys.exit()
