33c72751f9d0bd77b2a84d1b6bf0b8e641f56ef8
Farshid Tavakolizadeh <farshid.tz@gmail.com>
2017-01-17 14:10:23 +0100

    adding backend script as flag

--- docker/pyroAdapter.py
+++ docker/pyroAdapter.py
@@ -3,21 +3,12 @@
  PyroAdapter exposes an external learning module via Pyro4
 """
 import Pyro4
-import sys
-import imp
-import os
-import time
+import sys, imp, os, getopt
+
 
 class PyroAdapter(object):
-    @Pyro4.expose
-    def init(self, backend):
-        print("backend: %s" % backend)
-        # Add directory of backend script to resolve local modules with relative paths
-        backendDir = os.path.dirname(backend["script"])
-        sys.path.append(backendDir)
-
-        module = imp.load_source(backend["name"], backend["script"])
-        self.backend = getattr(module, backend["name"])()
+    def __init__(self):
+        self.backend = backendModule
 
     @Pyro4.expose
     def build(self, classifier):
@@ -52,19 +43,64 @@ class PyroAdapter(object):
         self.backend.importModel(model)
 
 
-# Start Pyro
-Pyro4.config.SERIALIZER = 'pickle'
-daemon = Pyro4.Daemon()
-
-try:
-    ns = Pyro4.locateNS()
+def startPyro(args):
+    Pyro4.config.SERIALIZER = 'pickle'
+    daemon = Pyro4.Daemon()
     uri = daemon.register(PyroAdapter)
-    ns.register("learning-agent", uri)
-    # TODO: Print the uri as a JSON string
-    print(uri) # NOTE: This is read by the parent process.
-except Exception as e:
-    print("Exception: {}".format(e))
-    raise SystemExit
+    if (args["nameserver"]):
+        try:
+            ns = Pyro4.locateNS()
+            ns.register("learning-agent", uri)
+        except Exception as e:
+            print("Exception: {}".format(e))
+            raise SystemExit
+
+    # e.g. uri: PYRO:obj_73fcc95930ed45caacba17be6bdbce74@localhost
+    print(uri)  # NOTE: This is read by the parent process.
+    daemon.requestLoop()
 
 
-daemon.requestLoop()
+def parseArgs(argv):
+    mandatoryArgs = ['bname', 'bpath']
+    parsed = {'bname': None, 'bpath': None, 'nameserver': False}
+
+    def help(exitcode):
+        print 'Usage: pyroAdapter.py --bname=<backend-name> --bpath=<backend-path> --ns'
+        sys.exit(exitcode)
+
+    try:
+        opts, args = getopt.getopt(argv, "h", ["bname=", "bpath=", "ns"])
+    except getopt.GetoptError:
+        help(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            help(0)
+        elif opt == "--bname":
+            parsed["bname"] = arg
+        elif opt == "--bpath":
+            parsed["bpath"] = arg
+        elif opt == '--ns':
+            parsed["nameserver"] = True
+
+    # check mangatory args
+    for k in parsed:
+        if k in mandatoryArgs and parsed[k] is None:
+            help(2)
+
+    return parsed
+
+
+def main(args):
+    # Add directory of backend script to resolve local modules with relative paths
+    backendDir = os.path.dirname(args["bpath"])
+    sys.path.append(backendDir)
+
+    module = imp.load_source(args["bname"], args["bpath"])
+    global backendModule
+    backendModule = getattr(module, args["bname"])()
+
+    startPyro(args)
+
+
+if __name__ == "__main__":
+    main(parseArgs(sys.argv[1:]))
