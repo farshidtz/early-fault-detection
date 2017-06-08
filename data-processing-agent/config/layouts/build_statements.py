1542ddf8856bdaa8c3c19aa4c058989ddf8570ff
Farshid Tavakolizadeh <email@farshid.ws>
2017-06-08 11:21:24 +0200

    corrected layout information

--- data-processing-agent/config/layouts/build_statements.py
+++ data-processing-agent/config/layouts/build_statements.py
@@ -1,26 +1,28 @@
 import json
+import copy
 
 STATEMENT_MASTER_FILE = 'statement_master.json'
 product_types = ["ABU1", "ABU2", "ABU3", "ABU4", "ABU5", "ABU6"]
 
-with open(STATEMENT_MASTER_FILE) as data_file:    
+with open(STATEMENT_MASTER_FILE) as data_file:
     print "Loading " + STATEMENT_MASTER_FILE
-    statement = json.load(data_file)
+    statement_master = json.load(data_file)
 
 # build and export statement object files
 bootstrapping = []
 for x in product_types:
-    with open(x+"_layout.json") as statement_data_file:        
+    statement = copy.deepcopy(statement_master)
+    with open(x+"_layout.json") as statement_data_file:
         print "Loading " + x+"_layout.json"
         layout = json.load(statement_data_file)
         statement["Model"]["Parameters"]["Classifier"]["production_layout"] = layout
         statement["Name"] = layout["type"]
         with open('generated_'+x+'.json', 'w') as outfile:
             print 'Saving generated_'+x+'.json'
-            json.dump(statement, outfile, indent=2, separators=(',', ': '))
+            json.dump(statement, outfile, indent=2, sort_keys=True)
         bootstrapping.append(statement)
 
 # build and export bootstrapping file
 with open('generated_bootstrapping.json', 'w') as outfile:
     print 'Saving generated_bootstrapping.json'
-    json.dump(bootstrapping, outfile, indent=2, separators=(',', ': '))
+    json.dump(bootstrapping, outfile, indent=2, sort_keys=True)
