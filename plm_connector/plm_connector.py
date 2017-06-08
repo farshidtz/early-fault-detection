f49f5ec75b2aa3f8032d222ffdb1fb578caa8559
Farshid Tavakolizadeh <email@farshid.ws>
2017-06-08 17:21:07 +0200

    sending positive feedbacks

--- plm_connector/plm_connector.py
+++ plm_connector/plm_connector.py
@@ -40,14 +40,15 @@ def outgoingHandler(json_obj):
     mcc = model['Evaluator']['WindowEvaluators'][0]['evaluationAlgorithms']['MatthewsCorrelationCoefficient']['result']
     scm = model['Evaluator']['WindowEvaluators'][0]['sequentialConfusionMatrix'][0]
     logger.debug("{}: Prediction for {} with MCC:{:0.2f} SCM:{} -> {}".format(model['Name'], result['originalInput']['id'], mcc, scm, result['prediction']))
-    if result['prediction']==1:
-	id = result['originalInput']['id']
-	type = result['originalInput']['type']
-	s = id.split('/')
-	s.insert(2, type)
-	id = '/'.join(s)
-        logger.debug("->PLM {}".format(id))
-        sock_writer.send(id)
+    #if result['prediction']==1:
+    id = result['originalInput']['id']
+    type = result['originalInput']['type']
+    s = id.split('/')
+    s.insert(2, type)
+    s.insert(len(s)-1, str(result['prediction']))
+    id = '/'.join(s)
+    logger.debug("->PLM {}".format(id))
+    sock_writer.send(id)
 
 def incomingHandler(json_obj):
     #logger.debug("->Broker {}".format(json_obj))
